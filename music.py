1ade51050f9d86688757da5604f90431659afb14
Aryan Chaurasia <36154235+kulterryan@users.noreply.github.com>
2021-01-19 23:36:29 +0530

    updated

--- music.py
+++ music.py
@@ -4,13 +4,27 @@ import discord
 from discord.ext import commands
 import os # for importing token
 
-client = commands.bot(command_prefix="$") # setting prefix for bot
+client = discord.Client() # defining Discord Bot
 
-# creating play command
-@create.command()
-async def play(ctx, url:str): # for entering url
-    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='General') # to access voice channel data
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild) # voice client
-    await voiceChannel.connect()
+starter_encouragements = [
+    "Cheer up!",
+    "Hang in there.",
+    "Hey! You know what, You're great, cheer up and f**k the world."
+]
+
+@client.event # When user logged in Discord Server
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+# Getting Started
+@client.event # When user send messages on Discord Server
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    msg = message.content # short substitution
+
+    if msg.startswith('hello'):
+        await message.channel.send("Hello! This is BotKage: The Ninja Bot.\nBuilt by github.com/kulterryan") # Sending msg as Reponse
 
 client.run(os.getenv('TOKEN')) # accessing Discord Bot
